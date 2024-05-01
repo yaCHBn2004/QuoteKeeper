6da1885214e21c832a9dde4fd7e6f470796c4fde
Cheurfi Belhadj Yassine <91964469+yaCHBn2004@users.noreply.github.com>
2024-05-01 10:05:20 +0200

    feat: tags list

--- src/App.js
+++ src/App.js
@@ -14,7 +14,6 @@ function App() {
     <SideBar/>
     <Routes>
         <Route path="/" element={<Home/>} />
-
         <Route path="/Quotes" element={<Quotes/>} />
     </Routes>
         
--- src/component/Quotes.js
+++ src/component/Quotes.js
@@ -1,10 +1,29 @@
+import { useState } from "react";
 import NavBar from "./NavBar.js"
+import "./Quotes.css";
 const Quotes = () => {
+const [tags , setTags] = useState([
+    "Life" , "Design" , "insperational" , "Philosoghy"
+])
     return ( 
 
         <div className="Quotes">
             <NavBar/>
-            <h1>Quotes</h1>
+            <h1>All quotes</h1>
+            <div className="tags">
+            {tags.map((tag)=>(
+                <div className="tag">
+                    <h1>#{tag}</h1>
+                </div>
+            ))}
+            </div>
+
+
+            
+
+
+
+
         </div>
      );
 }
